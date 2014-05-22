5e912c3430471afd440a39b67926c24a86114154
edom18 <edo.m18@gmail.com>
2014-05-22 14:30:26 +0900

    Initial Commit

---
+++ SplitViewControllerTest/SVTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVTAppDelegate.h
+//  SplitViewControllerTest
+//
+//  Created by 比留間 和也 on 2014/05/22.
+//  Copyright (c) 2014年 比留間 和也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SplitViewControllerTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SplitViewControllerTest
+//
+//  Created by 比留間 和也 on 2014/05/22.
+//  Copyright (c) 2014年 比留間 和也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVTAppDelegate class]));
+    }
+}
